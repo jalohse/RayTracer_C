89f5b931012f4c065ceb52d9c21207d5722ae28c
Jessica Hair <jessicaalohse@gmail.com>
2018-07-02 15:00:28 -0400

    Create camera to allow for multiple samples per pixel

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include "sphere.h"
 #include "hitable_list.h"
+#include "camera.h"
 using namespace std;
 
 vec3 getColor(const ray &ray, hitable *world) {
@@ -17,23 +18,25 @@ vec3 getColor(const ray &ray, hitable *world) {
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     ofstream file;
     file.open("example.ppm");
     file << "P3\n" << nx << " " << ny << "\n255\n";
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
     hitable *list[2];
     list[0] = new sphere(vec3(0,0,-1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
     hitable *world = new hitable_list(list, 2);
+    camera camera;
     for (int j = ny-1; j >= 0 ; j--) {
         for (int i = 0; i < nx; ++i) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            vec3 color = getColor(r, world);
+            vec3 color(0,0,0);
+            for(int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray camRay = camera.get_ray(u, v);
+                color += getColor(camRay, world);
+            }
+            color /= float(ns);
             int ir = int(255.99 * color[0]);
             int ig = int(255.99 * color[1]);
             int ib = int(255.99 * color[2]);
