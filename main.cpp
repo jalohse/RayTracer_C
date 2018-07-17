804c0b8f613c957574db9aed368c7d8824b82337
Jessica Hair <jessicaalohse@gmail.com>
2018-07-17 14:45:53 -0400

    Add refraction

--- main.cpp
+++ main.cpp
@@ -18,6 +18,59 @@ vec3 reflect(const vec3& vector, const vec3& normal) {
     return vector - 2 * dot(vector, normal) * normal;
 }
 
+bool refract(const vec3& vector, const vec3& normal, float ni_over_nt, vec3& refracted) {
+    vec3 uv = unit_vector(vector);
+    float dt = dot(uv, normal);
+    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt * (uv - normal * dt) - normal * sqrt(discriminant);
+        return true;
+    }
+    return false;
+}
+
+float schlick(float cosine, float reflection_index) {
+    float r0 = (1 - reflection_index) / (1 + reflection_index);
+    r0 = r0 * r0;
+    return  r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
+class dielectric: public material {
+public:
+    dielectric(float ri): reflection_index(ri) {}
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        vec3 outward_normal;
+        vec3 reflected = reflect(r_in.direction(), rec.normal);
+        float ni_over_nt;
+        attenuation = vec3(1.0, 1.0, 1.0);
+        vec3 refracted;
+        float reflect_probability;
+        float cosine;
+        if(dot(r_in.direction(), rec.normal) > 0) {
+            outward_normal = -rec.normal;
+            ni_over_nt = reflection_index;
+            cosine = reflection_index * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        } else {
+            outward_normal = rec.normal;
+            ni_over_nt = 1.0 / reflection_index;
+            cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        }
+        if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+            reflect_probability = schlick(cosine, reflection_index);
+        } else {
+            scattered = ray(rec.position, reflected);
+            reflect_probability = 1.0;
+        }
+        if(drand48() < reflect_probability) {
+            scattered = ray(rec.position, reflected);
+        } else {
+            scattered = ray(rec.position, refracted);
+        }
+        return true;
+    }
+    float reflection_index;
+};
+
 class metal : public material {
 public:
     metal(const vec3& a, float f): albedo(a) { if (f< 1) fuzz = f; else fuzz =1;};
@@ -66,12 +119,13 @@ int main() {
     ofstream file;
     file.open("example.ppm");
     file << "P3\n" << nx << " " << ny << "\n255\n";
-    hitable *list[4];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    hitable *list[5];
+    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 1.0));
-    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.3));
-    hitable *world = new hitable_list(list, 4);
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0));
+    list[3] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+    hitable *world = new hitable_list(list, 5);
     camera camera;
     for (int j = ny-1; j >= 0 ; j--) {
         for (int i = 0; i < nx; ++i) {
