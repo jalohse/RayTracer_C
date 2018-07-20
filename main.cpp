84d5ed268499dd514c7b6a037b2c3e3582ee514e
Jessica Hair <jessicaalohse@gmail.com>
2018-07-20 12:43:17 -0400

    Move material classes to material.h and add code for multithreading. Need to restructure image output to use multithreading

--- main.cpp
+++ main.cpp
@@ -1,100 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <thread>
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
 #include "material.h"
 using namespace std;
 
-vec3 random_in_unit_sphere() {
-    vec3 p;
-    do {
-        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1,1,1);
-    } while (p.squared_length() >= 1.0);
-    return p;
-}
+int nx = 200;
+int ny = 100;
+int ns = 100;
 
-vec3 reflect(const vec3& vector, const vec3& normal) {
-    return vector - 2 * dot(vector, normal) * normal;
-}
-
-bool refract(const vec3& vector, const vec3& normal, float ni_over_nt, vec3& refracted) {
-    vec3 uv = unit_vector(vector);
-    float dt = dot(uv, normal);
-    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
-    if (discriminant > 0) {
-        refracted = ni_over_nt * (uv - normal * dt) - normal * sqrt(discriminant);
-        return true;
-    }
-    return false;
-}
-
-float schlick(float cosine, float reflection_index) {
-    float r0 = (1 - reflection_index) / (1 + reflection_index);
-    r0 = r0 * r0;
-    return  r0 + (1 - r0) * pow((1 - cosine), 5);
-}
-
-class dielectric: public material {
+class PixelIterator {
 public:
-    dielectric(float ri): reflection_index(ri) {}
-    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-        vec3 outward_normal;
-        vec3 reflected = reflect(r_in.direction(), rec.normal);
-        float ni_over_nt;
-        attenuation = vec3(1.0, 1.0, 1.0);
-        vec3 refracted;
-        float reflect_probability;
-        float cosine;
-        if(dot(r_in.direction(), rec.normal) > 0) {
-            outward_normal = -rec.normal;
-            ni_over_nt = reflection_index;
-            cosine = reflection_index * dot(r_in.direction(), rec.normal) / r_in.direction().length();
-        } else {
-            outward_normal = rec.normal;
-            ni_over_nt = 1.0 / reflection_index;
-            cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    PixelIterator () { Init(); };
+    void Init() { ix = 0; };
+    bool GetPixel(int &x, int &y) {
+        int i = ix++;
+        if( i > nx * ny) {
+            return false;
         }
-        if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
-            reflect_probability = schlick(cosine, reflection_index);
-        } else {
-            scattered = ray(rec.position, reflected);
-            reflect_probability = 1.0;
-        }
-        if(drand48() < reflect_probability) {
-            scattered = ray(rec.position, reflected);
-        } else {
-            scattered = ray(rec.position, refracted);
-        }
+        y = i / nx;
+        x = i - y * nx;
         return true;
-    }
-    float reflection_index;
+    };
+private:
+    atomic<int> ix;
 };
 
-class metal : public material {
-public:
-    metal(const vec3& a, float f): albedo(a) { if (f< 1) fuzz = f; else fuzz =1;};
-    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.position, reflected + fuzz * random_in_unit_sphere());
-        attenuation = albedo;
-        return dot(scattered.direction(), rec.normal) > 0;
-    }
-    vec3 albedo;
-    float fuzz;
-};
-
-class lambertian: public material {
-public:
-    lambertian(const vec3& a): albedo(a) {};
-    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-        vec3 target = rec.position + rec.normal + random_in_unit_sphere();
-        scattered = ray(rec.position, target - rec.position);
-        attenuation = albedo;
-        return true;
-    }
-    vec3 albedo;
-};
+PixelIterator pixel_iterator;
 
 vec3 getColor(const ray &r, hitable *world, int depth) {
     hit_record record;
@@ -141,15 +75,31 @@ hitable *random_scene() {
     return new hitable_list(list, i);
 }
 
+void RenderPixels() {
+    int x, y;
+    while(pixel_iterator.GetPixel(x,y)) {
+
+    }
+}
+
+void BeginRender() {
+    int n = thread::hardware_concurrency();
+    if(n <= 0) {
+        n = 1;
+    }
+    pixel_iterator.Init();
+    for (int i = 0; i < n; ++i) {
+        thread(RenderPixels).detach();
+    }
+
+}
+
 int main() {
-    int nx = 200;
-    int ny = 100;
-    int ns = 100;
     ofstream file;
     file.open("example.ppm");
     file << "P3\n" << nx << " " << ny << "\n255\n";
     hitable *world = random_scene();
-    vec3 look_from(15, 15, 0);
+    vec3 look_from(15, 0, 0);
     vec3 look_at(0, 0, -1);
     float distance_to_focus = (look_from - look_at).length();
     float aperature = 2.0;
--- material.h
+++ material.h
@@ -8,5 +8,96 @@ public:
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
 };
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1,1,1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
+vec3 reflect(const vec3& vector, const vec3& normal) {
+    return vector - 2 * dot(vector, normal) * normal;
+}
+
+bool refract(const vec3& vector, const vec3& normal, float ni_over_nt, vec3& refracted) {
+    vec3 uv = unit_vector(vector);
+    float dt = dot(uv, normal);
+    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt * (uv - normal * dt) - normal * sqrt(discriminant);
+        return true;
+    }
+    return false;
+}
+
+float schlick(float cosine, float reflection_index) {
+    float r0 = (1 - reflection_index) / (1 + reflection_index);
+    r0 = r0 * r0;
+    return  r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
+class dielectric: public material {
+public:
+    dielectric(float ri): reflection_index(ri) {}
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        vec3 outward_normal;
+        vec3 reflected = reflect(r_in.direction(), rec.normal);
+        float ni_over_nt;
+        attenuation = vec3(1.0, 1.0, 1.0);
+        vec3 refracted;
+        float reflect_probability;
+        float cosine;
+        if(dot(r_in.direction(), rec.normal) > 0) {
+            outward_normal = -rec.normal;
+            ni_over_nt = reflection_index;
+            cosine = reflection_index * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        } else {
+            outward_normal = rec.normal;
+            ni_over_nt = 1.0 / reflection_index;
+            cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        }
+        if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+            reflect_probability = schlick(cosine, reflection_index);
+        } else {
+            scattered = ray(rec.position, reflected);
+            reflect_probability = 1.0;
+        }
+        if(drand48() < reflect_probability) {
+            scattered = ray(rec.position, reflected);
+        } else {
+            scattered = ray(rec.position, refracted);
+        }
+        return true;
+    }
+    float reflection_index;
+};
+
+class metal : public material {
+public:
+    metal(const vec3& a, float f): albedo(a) { if (f< 1) fuzz = f; else fuzz =1;};
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.position, reflected + fuzz * random_in_unit_sphere());
+        attenuation = albedo;
+        return dot(scattered.direction(), rec.normal) > 0;
+    }
+    vec3 albedo;
+    float fuzz;
+};
+
+class lambertian: public material {
+public:
+    lambertian(const vec3& a): albedo(a) {};
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        vec3 target = rec.position + rec.normal + random_in_unit_sphere();
+        scattered = ray(rec.position, target - rec.position);
+        attenuation = albedo;
+        return true;
+    }
+    vec3 albedo;
+};
+
+
 
 #endif //RAYTRACER_MATERIAL_H
